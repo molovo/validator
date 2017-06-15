3f59a0d1f497e4734d793c655d43d7ea1c6689ff
James Dinsdale <hi@molovo.co>
2017-06-15 23:52:19 +0100

    Make tests pass and enforce 100% coverage

--- test/rules/regex.js
+++ test/rules/regex.js
@@ -10,5 +10,16 @@ test('passes value with letters only', t => {
 test('fails value containing non-letter characters', t => {
   const rule = new Regex(new Validator(), ['^[a-z]+$'])
   t.false(rule.validate('test123'))
+})
+
+test('prints error message', t => {
+  const rule = new Regex(new Validator(), ['^[a-z]+$'])
   t.is(rule.message({name: 'test', label: 'Test'}), 'The value for Test failed to match the regex /^[a-z]+$/')
+})
+
+test('throws error if no regex is passed', t => {
+  const rule = new Regex(new Validator(), [])
+  const error = t.throws(() => rule.validate('test'))
+  t.is(error.name, 'InvalidRuleError')
+  t.is(error.message, 'You must provide a regular expression to compare against')
 })
--- test/validator.js
+++ test/validator.js
@@ -7,17 +7,34 @@ test('is valid without data', t => {
 })
 
 test('is valid without rules', t => {
-  const validator = new Validator({key: 'value'})
+  const validator = new Validator({name: 'value'})
   t.true(validator.validate())
 })
 
+test('parses rule arguments', t => {
+  const validator = new Validator({name: 'value'}, {name: 'required:1,2,3'})
+  t.deepEqual(validator.rules.name[0].args, ['1', '2', '3'])
+})
+
+test('throws error for invalid rule', t => {
+  const error = t.throws(() => new Validator({}, {name: 'not-a-rule'}))
+  t.is(error.name, 'InvalidRuleError')
+  t.is(error.message, 'Rule not-a-rule does not exist')
+})
+
 test('is valid when rules pass', t => {
-  const validator = new Validator({key: 'value'}, {key: 'required'})
+  const validator = new Validator({name: 'value'}, {name: 'required'})
   t.true(validator.validate())
 })
 
 test('stores error messages for failures', t => {
-  const validator = new Validator({key: null}, {key: 'required'})
+  const validator = new Validator({name: ''}, {name: 'required'})
   t.false(validator.validate())
-  t.deepEqual(validator.errors, {key: 'Key is required'})
+  t.deepEqual(validator.errors, {name: 'Name is required'})
+})
+
+test('reports custom error message', t => {
+  const validator = new Validator({name: ''}, {name: 'required'}, {name: 'This is a custom error message'})
+  t.false(validator.validate())
+  t.deepEqual(validator.errors, {name: 'This is a custom error message'})
 })
